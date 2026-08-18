59106963cb35a6d98f34a98e3db87a0bb2f6d17a
jaredvdb <jared.vandenbosch@gmail.com>
2026-08-18 15:45:11 -0400

    Update

--- SQL/01-exploration/Inspect-SpecObj.sql
+++ SQL/01-exploration/Inspect-SpecObj.sql
@@ -18,3 +18,4 @@ SELECT TOP 5000
     ra,               -- Right ascension
     dec               -- Declination 
 FROM SpecObj
+WHERE zWarning = 0 AND class = "STAR" -- only Stars, with cleaner data 
